63e013ab8043ff9050cbff95adf549d79357c7a2
Oscar Leon <oscar9992018@gmail.com>
2024-02-25 16:50:20 -0300

    initial commit

---
+++ src/main/java/com/example/leonDemo1/LeonDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.leonDemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeonDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/leonDemo1/LeonDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leonDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
